229ba63068106c007842dee4dd493dfa6f98f2fc
tihangrai <tihangrai@gmail.com>
2020-09-12 01:08:41 -0400

    fixed typo

--- utils/validators.js
+++ utils/validators.js
@@ -10,7 +10,7 @@ module.exports.validateRegisterInput = (
   }
 
   if (email.trim() === "") {
-    errors.username = "Username must not me empty";
+    errors.email = "Email must not me empty";
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
     if (!email.match(regEx)) {
